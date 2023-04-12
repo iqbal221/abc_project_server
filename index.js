7e9426d23206b5b00b3f83263c802445bdf02922
zahed iqbal <tajiniiuc222@gmail.com>
2023-04-12 15:39:22 +0600

    source changes to index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const routes = require('./src/Routes/Api')
+const colors = require('@colors/colors');
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 
 const app = express()
@@ -21,6 +24,18 @@ app.use('/api/v1/',routes)
 // Not found route
 app.get('*',(req,res)=>{
     res.send('404 Not Found')
+})
+
+// database connection
+mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log(`database connected successfully`.blue)
+})
+
+// server port
+const port = process.env.PORT || 8000;
+
+app.listen(port,()=>{
+    console.log(`server is running at ${port}`.red)
 })
 
 
